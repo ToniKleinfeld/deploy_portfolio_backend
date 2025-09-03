db226f3bcf5d0383b06b28fe15d3727f9603fc12
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-09-03 13:51:31 +0200

    Add PostgreSQL readiness check to entrypoint script for improved deployment reliability

--- backend.Coderr/entrypoint.deploy.sh
+++ backend.Coderr/entrypoint.deploy.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Wait for PostgreSQL $DB_HOST:$DB_PORT..."
+
+# -q for "quiet" (no output except errors)
+# The loop runs as long as pg_isready is *not* successful. (Exit-Code != 0)
+while ! pg_isready -h "$DB_HOST" -p "$DB_PORT" -q; do
+  echo "PostgreSQL is unavailable - sleep for 1 second"
+  sleep 1
+done
+
+echo "PostgreSQL is ready - continue..."
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
