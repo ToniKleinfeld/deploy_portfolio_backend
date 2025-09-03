c64cf936f57ca6a50901fde7dfba8e9c0c2875b7
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-09-03 13:45:31 +0200

    Remove makemigrations command from entrypoint scripts to streamline deployment process

--- Videoflix/entrypoint.deploy.sh
+++ Videoflix/entrypoint.deploy.sh
@@ -12,7 +12,6 @@ done
 
 echo "PostgreSQL is ready - continue..."
 
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
--- backend.Coderr/entrypoint.deploy.sh
+++ backend.Coderr/entrypoint.deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
